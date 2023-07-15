4a435205140dacf92100d2facb50c288dae92dfb
João Pedro Silva Cunha <jpcunha5@gmail.com>
2023-07-15 18:34:50 -0300

    feat: Consumindo API e mostrando dados no console | Pegando value da barra de pesquisa e gerando evento de clique no botão de busca

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,13 +1,21 @@
+type searchProps = {
+    loadUser: (userName: string) => Promise<void>;
+}
+
+import { useState } from 'react';
 import {BsSearch} from 'react-icons/bs'
 
-const Search = () => {
+const Search = ({loadUser}: searchProps) => {
+
+    const [userName, setUserName] = useState("");
+
     return (
         <div>
             <h2>Busque por um usuário:</h2>
             <p>Conheça seus melhores projetos</p>
             <div>
-                <input type="text" placeholder="Digite o nome do usuário"/>
-                <button>
+                <input type="text" placeholder="Digite o nome do usuário" onChange={(e)=>setUserName(e.target.value)}/>
+                <button onClick={() => loadUser(userName)}>
                     <BsSearch/>
                 </button>
             </div>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,22 @@
 import Search from "../components/search";
+import { useState } from "react";
+import { UserProps } from "../Types/user";
 
 const Home = () => {
+
+    const [user, setUser] = useState<UserProps | null> (null);
+
+    const loadUser = async (userName:string) => {
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+
+        const data = await res.json();
+
+        console.log(data);
+    }
+
     return (
         <div>
-            <Search/>
+            <Search loadUser={loadUser}/>
         </div>
     )
     
